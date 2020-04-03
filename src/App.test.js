82529ea380b712a9362144c16aafbea273e5b554
Adash <adash108@yahoo.com>
2020-04-03 15:29:38 +0100

    added testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 
-test('renders learn react link', () => {
+describe('App', () => {
+  test('test of the tests runner', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+test('renders learn Numpad link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Numpad/i);
   expect(linkElement).toBeInTheDocument();
 });
